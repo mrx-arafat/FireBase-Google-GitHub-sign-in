ec9c6ae54cc5ea93bcf067e883d3a194419add67
Easin Arafat <arafatmrx@gmail.com>
2021-10-10 09:34:58 +0600

    iniAuth

---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,10 @@
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCIKKY4Me2hiZ5ayEgcEppPke6P1HS22xw",
+  authDomain: "simple-firebase-authenti-1f046.firebaseapp.com",
+  projectId: "simple-firebase-authenti-1f046",
+  storageBucket: "simple-firebase-authenti-1f046.appspot.com",
+  messagingSenderId: "824823266189",
+  appId: "1:824823266189:web:cd8b003d2c1fe336329bc4",
+};
+export default firebaseConfig;
