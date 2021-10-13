60c95aaafa7e558e976a93f5843a7eda2870f427
Easin Arafat <arafatmrx@gmail.com>
2021-10-14 05:45:38 +0600

    init

--- src/App.js
+++ src/App.js
@@ -1,6 +1,22 @@
 import "./App.css";
+import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+import initializeAuthentication from "./Firebase/firebase.initialize";
+
+initializeAuthentication();
+
+const provider = new GoogleAuthProvider();
 
 function App() {
+  const handleGoogleSignIn = () => {
+    const auth = getAuth();
+    signInWithPopup(auth, provider).then((result) => {
+      // The signed-in user info.
+      const user = result.user;
+      // ...
+      console.log(user);
+    });
+  };
+
   return (
     <div className="App">
       <h1>Yo Bro</h1>
--- src/Firebase/firebase.initialize.js
+++ src/Firebase/firebase.initialize.js
@@ -1,6 +1,8 @@
 import { initializeApp } from "firebase/app";
 import firebaseConfig from "./firebase.config";
+import { GoogleAuthProvider } from "firebase/auth";
 
+const provider = new GoogleAuthProvider();
 const initializeAuthentication = () => {
   initializeApp(firebaseConfig);
 };
