ef4bf8772d53b2701808b526475862757bdd1127
Easin Arafat <arafatmrx@gmail.com>
2021-10-16 09:47:38 +0600

    commit after 2 days

--- src/App.js
+++ src/App.js
@@ -1,27 +1,90 @@
+import {
+  getAuth,
+  signInWithPopup,
+  GoogleAuthProvider,
+  GithubAuthProvider,
+  FacebookAuthProvider,
+  signOut,
+} from "firebase/auth";
+import { useState } from "react";
 import "./App.css";
-import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
 import initializeAuthentication from "./Firebase/firebase.initialize";
 
 initializeAuthentication();
 
-const provider = new GoogleAuthProvider();
+const googleProvider = new GoogleAuthProvider();
+const gitHubProvider = new GithubAuthProvider();
+const facebookProvider = new FacebookAuthProvider();
 
 function App() {
+  const [user, setUser] = useState({});
+  const auth = getAuth();
+
   const handleGoogleSignIn = () => {
-    const auth = getAuth();
-    signInWithPopup(auth, provider).then((result) => {
-      // The signed-in user info.
-      const user = result.user;
-      // ...
-      console.log(user);
+    signInWithPopup(auth, googleProvider)
+      .then((result) => {
+        const { displayName, email, photoURL } = result.user;
+        const loggedInUser = {
+          name: displayName,
+          email: email,
+          photo: photoURL,
+        };
+        setUser(loggedInUser);
+      })
+      .catch((error) => {
+        console.log(error.message);
+      });
+  };
+
+  const handleGithubSignIn = () => {
+    signInWithPopup(auth, gitHubProvider).then((result) => {
+      const { displayName, photoURL, email } = result.user;
+      const loggedInUser = {
+        name: displayName,
+        email: email,
+        photo: photoURL,
+      };
+      setUser(loggedInUser);
     });
   };
 
+  const handleFacebookSignIn = () => {
+    signInWithPopup(auth, facebookProvider).then((result) => {
+      const { displayName, photoURL, email } = result.user;
+      console.log(result.user);
+      const loggedInUser = {
+        name: displayName,
+        email: email,
+        photo: photoURL,
+      };
+      setUser(loggedInUser);
+    });
+  };
+
+  const handleSignOut = () => {
+    signOut(auth).then(() => {
+      setUser({});
+    });
+  };
   return (
     <div className="App">
-      <h1>Yo Bro</h1>
-
-      <button onClick={handleGoogleSignIn}>google sign in</button>
+      {!user.name ? (
+        <div>
+          <button onClick={handleGoogleSignIn}>Google Sign In</button>
+          <button onClick={handleGithubSignIn}>Github Sign In</button>
+          <button onClick={handleFacebookSignIn}>Facebook Sign In</button>
+        </div>
+      ) : (
+        <button onClick={handleSignOut}>Sign Out</button>
+      )}
+      <br />
+      {user.name && (
+        <div>
+          <h2>Welcome {user.name}</h2>
+          <p>I know your email address: {user.email}</p>
+          <img src={user.photo} alt="" />
+        </div>
+      )}
     </div>
   );
 }
